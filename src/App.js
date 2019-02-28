c7717d774217e4dc285d5b955095e68193e74d8e
Jason Roundtree <jasontree123@gmail.com>
2019-02-27 21:33:30 -0800

    Setup basic CSS grid outline for horizontal player board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import AppBar from '@material-ui/core/AppBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <AppBar className="AppBar">
+          <header>Hanamikoji Card Game Replica</header>
+        </AppBar>
+
+        <div className="container">
+          <div className="playArea1">Player 1 area</div>
+          <div className="playerActions1">Player 1 actions</div>
+          <div className="geishaCards">Geisha Cards</div>
+          <div className="drawPile">D</div>
+          <div className="playerActions2">Player 2 actions</div>
+          <div className="playArea2">Player 2 area</div>
+        </div>
       </div>
     );
   }
