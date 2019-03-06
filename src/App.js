0c6f64564d2bbab623a4dff135785d24699292d8
Jason Roundtree <jasontree123@gmail.com>
2019-03-05 19:05:29 -0800

    Setup action and hand sections to expand full width

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ class App extends Component {
   render() {
     return (
       <div>
-        <AppBar className="AppBar">
+        <AppBar className="top-header">
           <header>Hanamikoji Card Game Replica</header>
         </AppBar>
 
         <div className="grid-container">
-          <div className="playerHand1">Play Area/Hand</div>
-          <div className="playerActions1">Player 1 actions</div>
-          <div className="playArea1">Player 1 Item Cards</div>
+
+          <div className="playerHand1">
+            Play Area/Hand
+          </div>
+
+          <div className="playerActions1">
+            Player 1 actions
+          </div>
+
+          <div className="playArea1">
+            Player 1 Item Cards
+          </div>
+
           <div className="geishaCards flex-container">
             <div>A</div>
             <div>B</div>
@@ -24,12 +34,22 @@ class App extends Component {
             <div>G</div>
             <div>H</div>
           </div>
-          <div className="drawPile">Draw</div>
-          <div className="playerHand2">Play Area/Hand</div>
-          <div className="playerActions2">Player 2 actions</div>
-          <div className="playArea2">Player 2 Item Cards</div>
-          
-          
+          <div className="drawPile flex-container">
+            <div>Draw</div>
+          </div>
+
+          <div className="playerHand2">
+            Play Area/Hand
+          </div>
+
+          <div className="playerActions2">
+            Player 2 actions
+          </div>
+
+          <div className="playArea2">
+            Player 2 Item Cards
+          </div>
+
         </div>
       </div>
     );
